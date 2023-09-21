5ab2767c67d757d4aa22d831cc6971feac29c06a
semaed <figsaned@gmail.com>
2023-09-21 16:18:55 -0400

    Adding a comment

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
+//Reads and prints the content of a file
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, text) => {
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    console.log(text.toString().trim());
+    console.log(data);
   }
 });
